f534481aa787cb8cad38e5865b6d4adc58194be8
anithaamarnath <anithaamarnath04@gmail.com>
2019-03-16 06:10:59 +0000

    even.js exercise wirht 'IsOdd' function completed

---
+++ odd.js
@@ -0,0 +1,9 @@
+function isOdd(num) {
+  return num % 3 === 0;
+}
+
+ // var tenIsEven = isEven(10);
+ // var elevenIsEven = isEven(11);
+
+ console.log("3 is Odd: " + isOdd(3));
+ console.log("8 is Odd: " + isOdd(8));
