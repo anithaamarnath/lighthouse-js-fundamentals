8c8b0317abf6a3bef692df8ff954dae46546eb95
anithaamarnath <anithaamarnath04@gmail.com>
2019-03-18 06:18:47 +0000

    Add conditinals exercises

---
+++ go-outside.js
@@ -0,0 +1,12 @@
+var temparature  = -1;
+//var cold = false;
+if(temparature<0) {
+  console.log("Make sure you pick out a scarf!");
+}else
+if(temparature < 15){
+  console.log("Short sleeves wont cut it!");
+
+}else{
+  console.log("Short sleeves are fine");
+}
+console.log("Now you're ready to go outside");
